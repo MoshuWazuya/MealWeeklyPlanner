e253571a5e36a9edf92ad173b1b1195e2be9209b
wazir <wazirnordin51@gmail.com>
2025-01-12 03:11:09 +0800

    Inserted AddMealUI

--- src/main/java/com/example/mealplannerweekly/LoginPageUI.java
+++ src/main/java/com/example/mealplannerweekly/LoginPageUI.java
@@ -14,6 +14,12 @@ import org.example.loginpage.MainMenuUI;
 
 import java.io.*;
 
+/**
+ *
+ * @author Wazir
+ * Mohamad Wazir bin Nordin 2215353
+ */
+
 
 public class LoginPageUI extends Application  {
     @Override
